7f3bf0c2e06a8f3ba69f2008dc7e8567c613d901
rajesh <peketirajesh007@gmail.com>
2024-02-04 17:31:39 +0530

    hai

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,35 +1,35 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// const clientUrl = "http://localhost:3000/"
+const clientUrl = "http://localhost:3000/"
 
-// const routes = {
-//     chat: clientUrl + "chat",
-//     settings: clientUrl + "settings",
-//     search: clientUrl + "search",
-//     profile: clientUrl + "profile",
-//     searchUser: clientUrl + '/user/:[id]'
-// }
+const routes = {
+    chat: clientUrl + "chat",
+    settings: clientUrl + "settings",
+    search: clientUrl + "search",
+    profile: clientUrl + "profile",
+    searchUser: clientUrl + '/user/:[id]'
+}
 
-// export async function middleware(req) {
-//     const url = req.url
-//     const token = req.cookies.has("token")
+export async function middleware(req) {
+    // const url = req.url
+    // const token = req.cookies.has("token")
 
-//     if (token && url === clientUrl) {
-//         return NextResponse.redirect(routes.chat)
-//     }
-//     if (!token && (
-//         url === routes.chat ||
-//         url === routes.profile ||
-//         url === routes.search ||
-//         url === routes.settings
-//     )) {
-//         return NextResponse.redirect(clientUrl);
-//     }
+    // if (token && url === clientUrl) {
+    //     return NextResponse.redirect(routes.chat)
+    // }
+    // if (!token && (
+    //     url === routes.chat ||
+    //     url === routes.profile ||
+    //     url === routes.search ||
+    //     url === routes.settings
+    // )) {
+    //     return NextResponse.redirect(clientUrl);
+    // }
 
-//     if (!token && url.includes("/user/")) {
-//         return NextResponse.redirect(clientUrl);
-//     }
-// }
+    // if (!token && url.includes("/user/")) {
+    //     return NextResponse.redirect(clientUrl);
+    // }
+}
 
 // export const config = {
 //     matcher: [
