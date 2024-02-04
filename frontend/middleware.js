8f546fddce48aa96ed992292d724f97fe12ddb10
rajesh <peketirajesh007@gmail.com>
2024-02-04 17:27:34 +0530

    gvagsas

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,46 +1,46 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-const clientUrl = "http://localhost:3000/"
+// const clientUrl = "http://localhost:3000/"
 
-const routes = {
-    chat: clientUrl + "chat",
-    settings: clientUrl + "settings",
-    search: clientUrl + "search",
-    profile: clientUrl + "profile",
-    searchUser: clientUrl + '/user/:[id]'
-}
+// const routes = {
+//     chat: clientUrl + "chat",
+//     settings: clientUrl + "settings",
+//     search: clientUrl + "search",
+//     profile: clientUrl + "profile",
+//     searchUser: clientUrl + '/user/:[id]'
+// }
 
-export async function middleware(req) {
-    const url = req.url
-    const token = req.cookies.has("token")
+// export async function middleware(req) {
+//     const url = req.url
+//     const token = req.cookies.has("token")
 
-    if (token && url === clientUrl) {
-        return NextResponse.redirect(routes.chat)
-    }
-    if (!token && (
-        url === routes.chat ||
-        url === routes.profile ||
-        url === routes.search ||
-        url === routes.settings
-    )) {
-        return NextResponse.redirect(clientUrl);
-    }
+//     if (token && url === clientUrl) {
+//         return NextResponse.redirect(routes.chat)
+//     }
+//     if (!token && (
+//         url === routes.chat ||
+//         url === routes.profile ||
+//         url === routes.search ||
+//         url === routes.settings
+//     )) {
+//         return NextResponse.redirect(clientUrl);
+//     }
 
-    if (!token && url.includes("/user/")) {
-        return NextResponse.redirect(clientUrl);
-    }
-}
+//     if (!token && url.includes("/user/")) {
+//         return NextResponse.redirect(clientUrl);
+//     }
+// }
 
-export const config = {
-    matcher: [
-        '/', '/chat', '/search', '/profile', '/user/:path*',
-        /*
-         * Match all request paths except for the ones starting with:
-         * - api (API routes)
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         */
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    ],
-}
+// export const config = {
+//     matcher: [
+//         '/', '/chat', '/search', '/profile', '/user/:path*',
+//         /*
+//          * Match all request paths except for the ones starting with:
+//          * - api (API routes)
+//          * - _next/static (static files)
+//          * - _next/image (image optimization files)
+//          * - favicon.ico (favicon file)
+//          */
+//         '/((?!api|_next/static|_next/image|favicon.ico).*)',
+//     ],
+// }
