e85410c37548d00569cb69c379e0fc609838e0ec
rajesh <peketirajesh007@gmail.com>
2024-02-04 17:43:47 +0530

    sbedh

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -11,36 +11,36 @@ const routes = {
 }
 
 export async function middleware(req) {
-    // const url = req.url
-    // const token = req.cookies.has("token")
+    const url = req.url
+    const token = req.cookies.has("token")
 
-    // if (token && url === clientUrl) {
-    //     return NextResponse.redirect(routes.chat)
-    // }
-    // if (!token && (
-    //     url === routes.chat ||
-    //     url === routes.profile ||
-    //     url === routes.search ||
-    //     url === routes.settings
-    // )) {
-    //     return NextResponse.redirect(clientUrl);
-    // }
+    if (token && url === clientUrl) {
+        return NextResponse.redirect(routes.chat)
+    }
+    if (!token && (
+        url === routes.chat ||
+        url === routes.profile ||
+        url === routes.search ||
+        url === routes.settings
+    )) {
+        return NextResponse.redirect(clientUrl);
+    }
 
-    // if (!token && url.includes("/user/")) {
-    //     return NextResponse.redirect(clientUrl);
-    // }
+    if (!token && url.includes("/user/")) {
+        return NextResponse.redirect(clientUrl);
+    }
 }
 
-// export const config = {
-//     matcher: [
-//         '/', '/chat', '/search', '/profile', '/user/:path*',
-//         /*
-//          * Match all request paths except for the ones starting with:
-//          * - api (API routes)
-//          * - _next/static (static files)
-//          * - _next/image (image optimization files)
-//          * - favicon.ico (favicon file)
-//          */
-//         '/((?!api|_next/static|_next/image|favicon.ico).*)',
-//     ],
-// }
+export const config = {
+    matcher: [
+        '/', '/chat', '/search', '/profile', '/user/:path*',
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+}
