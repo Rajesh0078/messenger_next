e86474463b37586891cab397ba37f838ffb610bc
rajesh <peketirajesh007@gmail.com>
2024-02-04 22:29:30 +0530

    mobile view set

--- frontend/components/BottomBar.jsx
+++ frontend/components/BottomBar.jsx
@@ -8,7 +8,7 @@ const BottomBar = () => {
 
     const navigate = useRouter()
     return (
-        <div className='fixed shadow-x z-100 flex justify-evenly md:hidden -    -bottom-1 text-blue-900 bg-white  w-full py-3 text-[1.5rem]'>
+        <div className='fixed shadow-x z-100 flex justify-evenly md:hidden pb-5 -bottom-1 text-blue-900 bg-white  w-full pt-4 text-[1.5rem]'>
             <IoHome onClick={() => navigate.push("/profile", { scroll: false })} />
             <IoSearch onClick={() => navigate.push("/search", { scroll: false })} />
             <IoChatbubblesSharp onClick={() => navigate.push("/chat", { scroll: false })} />
