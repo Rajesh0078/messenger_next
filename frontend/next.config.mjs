a42005e6b8c8cef0e12c4ceadaa283bd27e5db1d
rajesh <peketirajesh007@gmail.com>
2024-03-06 10:44:24 +0530

    config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,14 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
     swcMinify: true,
-    images: {
-        remotePatterns: [
-            { protocol: 'https', hostname: 'vice.app', port: '', pathname: '/**/**/**' },
-            { protocol: 'https', hostname: 'vice-app.nyc3.digitaloceanspaces.com', port: '', pathname: '/**/**/**' },
-            { protocol: 'https', hostname: 'cdn-icons-png.flaticon.com', port: '', pathname: '/**/**/**' },
-        ],
-    },
+    // images: {
+    //     remotePatterns: [
+    //         { protocol: 'https', hostname: 'vice.app', port: '', pathname: '/**/**/**' },
+    //         { protocol: 'https', hostname: 'vice-app.nyc3.digitaloceanspaces.com', port: '', pathname: '/**/**/**' },
+    //         { protocol: 'https', hostname: 'cdn-icons-png.flaticon.com', port: '', pathname: '/**/**/**' },
+    //     ],
+    // },
 };
 
 export default nextConfig;
